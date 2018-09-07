6cd5e30f42c45f664e78ef0cf5a0615796e05b82
Dmitriy Dovgan <d_dovgan@wargaming.net>
2018-09-07 14:25:39 +0300

    master. Fix tests

--- tests/integration/components/image-drop-test.js
+++ tests/integration/components/image-drop-test.js
@@ -1,45 +1,47 @@
-import { findAll, find } from '@ember/test-helpers';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { findAll, find } from '@ember/test-helpers';
 
 
-moduleForComponent('image-drop', 'Integration | Component | image drop', {
-  integration: true
+module('Integration | Component | c alert', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    assert.expect(5);
+
+
+    await render(hbs`{{image-drop}}`);
+
+    assert.equal(findAll('.ember-image-drop').length, 1);
+    assert.equal(findAll('.ember-image-drop input').length, 1);
+    assert.equal(findAll('.ember-image-drop .text').length, 1);
+    assert.equal(findAll('.ember-image-drop .placeholder').length, 1);
+    assert.equal(findAll('.ember-image-drop .help-text').length, 1);
+  });
+
+  test('it renders', async function(assert) {
+    assert.expect(2);
+
+
+    await render(hbs`{{image-drop placeholder="banana" helpText="help!"}}`);
+    assert.equal(find('.ember-image-drop .placeholder').textContent, 'banana');
+    assert.equal(find('.ember-image-drop .help-text').textContent, 'help!');
+  });
+
+  test('background image updates with image', async function(assert) {
+    assert.expect(2);
+    this.set('image', 'data:fakeimagedata');
+
+    await render(hbs`{{image-drop image=image}}`);
+
+    let backgroundImage = this.$('.ember-image-drop').css('background-image');
+    assert.equal(backgroundImage, 'url("data:fakeimagedata")');
+
+    this.set('image', 'data:pancakes');
+    backgroundImage = this.$('.ember-image-drop').css('background-image');
+    assert.equal(backgroundImage, 'url("data:pancakes")');
+  });
 });
 
-test('it renders', function(assert) {
-  assert.expect(5);
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{image-drop}}`);
-
-  assert.equal(findAll('.ember-image-drop').length, 1);
-  assert.equal(findAll('.ember-image-drop input').length, 1);
-  assert.equal(findAll('.ember-image-drop .text').length, 1);
-  assert.equal(findAll('.ember-image-drop .placeholder').length, 1);
-  assert.equal(findAll('.ember-image-drop .help-text').length, 1);
-});
-
-test('text data is set correctly', function(assert) {
-  assert.expect(2);
-
-  this.render(hbs`{{image-drop placeholder="banana" helpText="help!"}}`);
-
-  assert.equal(find('.ember-image-drop .placeholder').textContent, "banana");
-  assert.equal(find('.ember-image-drop .help-text').textContent, "help!");
-});
-
-test('background image updates with image', function(assert) {
-  assert.expect(2);
-  this.set('image', 'data:fakeimagedata');
-  this.render(hbs`{{image-drop image=image}}`);
-
-  let backgroundImage = this.$('.ember-image-drop').css('background-image');
-  assert.equal(backgroundImage, "url(data:fakeimagedata)");
-
-  this.set('image', 'data:pancakes');
-  backgroundImage = this.$('.ember-image-drop').css('background-image');
-  assert.equal(backgroundImage, "url(data:pancakes)");
-});
